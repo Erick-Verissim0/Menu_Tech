fb60c3f0f2ae8a00636d15b41028b7ea6fe69d9e
erick.verissimo <erick.verissimo@teamsoft.com.br>
2023-10-08 02:11:21 -0300

    feat: add screen login/create user

--- front/src/components/LoginPage/index.tsx
+++ front/src/components/LoginPage/index.tsx
@@ -1,9 +1,46 @@
-import { Container } from "./styles";
+import React, { useState } from 'react';
+
+import {
+  LoginButton,
+  Fields,
+  Container,
+  ContainerFields,
+  Image,
+  ImageContainer,
+  Title,
+  CreateCountButton,
+  ChangeCollorButton,
+} from './styles';
+import loginPageImage from '../../assets/images/login-page.jpg';
 
 export function LoginPage() {
+  const [createCountMode, setCreateCountMode] = useState(false);
+  const [name, setName] = useState('');
+
+  const toggleCreateCount = () => {
+    setCreateCountMode(!createCountMode);
+  };
+
   return (
     <Container>
-      <h1>Olá</h1>
+      <ImageContainer>
+        <Image src={loginPageImage} alt="Minha Imagem" />
+      </ImageContainer>
+      <ContainerFields>
+        <Title>{createCountMode ? 'Crie uma Conta' : 'Faça o Login'}</Title>
+        {createCountMode && <Fields type="text" placeholder="Digite seu nome" value={name} onChange={(e) => setName(e.target.value)} />}
+
+        <Fields type="email" placeholder="Digite seu e-mail" />
+        <Fields type="password" placeholder="Digite sua senha" />
+        {createCountMode ? <LoginButton> Criar Conta </LoginButton> : <LoginButton>Entrar</LoginButton>}
+
+        <span>
+          {createCountMode ? 'Já possui conta? ' : 'Não tem uma conta? '}
+          <CreateCountButton onClick={toggleCreateCount}>
+            <ChangeCollorButton>{createCountMode ? 'Faça Login' : 'Criar Conta'}</ChangeCollorButton>
+          </CreateCountButton>
+        </span>
+      </ContainerFields>
     </Container>
-  )
+  );
 }
--- front/src/components/LoginPage/styles.ts
+++ front/src/components/LoginPage/styles.ts
@@ -1,12 +1,66 @@
 import styled from 'styled-components';
 
-export const Container = styled.header`
+export const Container = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
   height: 100vh;
+  background-color: #f0f2eb;
+`;
 
-  h1 {
-    color: black;
-  }
+export const ImageContainer = styled.div`
+  flex: 0 0 50%; /* A imagem ocupa 50% da largura da tela */
+  height: 100%;
+`;
+
+export const Image = styled.img`
+  width: 100%; /* Garante que a imagem ocupe toda a largura do container */
+  height: 100%; /* Mantém a proporção da imagem */
+`;
+
+export const ContainerFields = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 20vh;
+  color: black;
+`;
+
+export const Title = styled.h1`
+  font-size: 25px;
+  margin-bottom: 30px;
+`;
+
+export const Fields = styled.input`
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 15px;
+  border: none;
+  border-bottom: 2px solid #007bff;
+  border-color: #51445f;
+  background-color: transparent;
+  outline: none; /* Remove a borda ao focar */
+`;
+
+export const LoginButton = styled.button`
+  background-color: #51445f;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  margin-top: 15px;
+  margin-bottom: 25px;
+  cursor: pointer;
+`;
+
+export const ChangeCollorButton = styled.button`
+  color: #7a8c89;
+  border: none;
+  font-weight: 500;
+`;
+
+export const CreateCountButton = styled.button`
+  background-color: transparent;
+  color: #51445f;
+  border: none;
+  cursor: pointer;
 `;
